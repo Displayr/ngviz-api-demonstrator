039f8bc65b288492c3d434b3b0704217f1deba00
Francesco Ferraioli <francesco.ferraioli@outlook.com>
2022-07-28 15:24:54 +1000

    Update esbuild.js

--- esbuild.js
+++ esbuild.js
@@ -1,26 +1,7 @@
-var copy = require('esbuild-plugin-copy').default;
+const { esbuildConfigBuilder } = require('@displayr/ngviz');
+const copy = require('esbuild-plugin-copy').default;
+
 require('esbuild').build({
-    entryPoints: ['./src'], // Note that we build straight from TypeScript to get the best sourcemaps.  We still run tsc first because it does actual type checking.
-    bundle: true,
-    sourcemap: true,
-    format: 'cjs',  // Needed to produce something we can eval()
-    target: 'es6',
-    outfile: 'dist/index.js',
+    ...esbuildConfigBuilder(copy),
     external: ['jquery', 'plotly.js'],
-    plugins: [
-        copy({
-            verbose: false,
-            assets: {
-                from: ['./assets/*' ],
-                to: ['./assets'],
-            },
-        }),
-        copy({
-            verbose: false,
-            assets: {
-                from: ['ngviz.json' ],
-                to: ['.'],
-            },
-        }),
-    ],
-}).catch((err) => process.exit(1))
+}).catch(() => process.exit(1));
