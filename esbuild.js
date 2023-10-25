52b83ec0d49a7e9c2774092ab2823cd2a1fd88ad
Francesco Ferraioli <francesco.ferraioli@outlook.com>
2023-10-25 11:01:52 +1000

    Add `npm start`

--- esbuild.js
+++ esbuild.js
@@ -1,7 +1,9 @@
 const { commonEsbuildConfigForVisualizations } = require('./node_modules/@displayr/ngviz/commonEsbuildConfigForVisualizations.js');
+const { argv } = require('process');
 const copy = require('esbuild-plugin-copy').default;
 
 require('esbuild').build({
     ...commonEsbuildConfigForVisualizations(copy),
     external: ['jquery', 'plotly.js-dist-min'],
+    watch: argv[2] === "--watch",
 }).catch(() => process.exit(1));
