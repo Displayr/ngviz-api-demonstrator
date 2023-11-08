35762fd6eba5aa211d2a60fe9546eb444e74255c
Jeremy Chiu <jeremy.chiu@displayr.com>
2023-11-08 14:59:18 +1100

    FS2-1861: Use control creator

--- src/DataTab.ts
+++ src/DataTab.ts
@@ -1,4 +1,4 @@
-import { CheckBoxParams, ComboBoxParams, ControlCreator, DropBoxParams, IListModifiable, IObjectInspectorControl, IObjectInspectorSpecification, ListModifierParams } from '@displayr/ngviz';
+import { CheckBoxParams, ComboBoxParams, ControlCreator, ControlSet, DropBoxParams, IListModifiable, IObjectInspectorControl, IObjectInspectorSpecification, ListModifierParams } from '@displayr/ngviz';
 
 export type DataTabConfig = {
     inputSelector: DropBoxParams;
@@ -20,35 +20,34 @@ export function constructDataTab(
     config: DataTabConfig,
     settings: IObjectInspectorSpecification,
 ): DataTabControls {
+    const control_set = new ControlSet();
+    control_set.visibility = true;
     // This page will eventually be called 'Data' instead of 'Inputs'
     const page = 'Inputs';
+    const data_control_creator = new ControlCreator(settings, { page, group: 'Data Source' }, undefined, control_set);
+    const col_selection_control_creator = new ControlCreator(settings, { page, group: 'Column selection' }, undefined, control_set);
 
-    const primary_data = settings.dropBox({
+    const primary_data = data_control_creator.dropBox({
         name: 'primaryData',
         label: 'Data',
-        page,
-        group: 'Data Source',
         ...config.inputSelector,
     });
 
     let controls: DataTabControls = { inputSelector: primary_data };
 
     if (config?.switchRowColumn) {
-        const switch_rows = settings.checkBox({
+        const switch_rows = data_control_creator.checkBox({
             name: 'switchRowsColumns',
             label: 'Switch rows/columns',
-            page,
-            group: 'Data Source',
             ...config.switchRowColumn,
         });
         controls.switchRowColumn= switch_rows;
     }
 
     if (config?.showNetSum) {
-        const show_net_sum = settings.comboBox({
+        const show_net_sum = col_selection_control_creator.comboBox({
             name: 'showNetSum',
             label: 'Show Net/Sum',
-            page,
             group: 'Column selection',
             ...config.showNetSum,
         });
@@ -56,9 +55,8 @@ export function constructDataTab(
     }
 
     if (config?.labelModifier) {
-        const list_modifier = settings.listModifier({
+        const list_modifier = col_selection_control_creator.listModifier({
             name: 'listModifier',
-            page,
             group: 'Column selection',
             ...config.labelModifier,
         });
